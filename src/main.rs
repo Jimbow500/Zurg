6a72e4b9575416e9d6ab974783808052d3984507
Jimbow <19518954+Jimbow500@users.noreply.github.com>
2018-10-31 08:39:44 -0500

    Update main.rs
    Still unfinished but its all the work I can do when in school.

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ Make sub worlds like rooms that render on the map
 
 */
 
+#![allow(non_snake_case)]
 #![allow(unused)]
 use std::io::{self, Write};
-use std::{thread, time};
 use std::process::exit;
+use std::{thread, time};
 
 extern crate colored;
 use colored::*;
 
-
-fn main(){
+fn main() {
     println!("Welcome to Zurg");
     menu(false);
 }
@@ -30,23 +30,23 @@ struct Stats {
 struct Entity {
     //If yes it needs to be removed or garbage collected somehow.
     alive: bool,
-    
+
     name: String,
     health: u32,
     level: u32,
     ent_id: u8,
     // How many units they move per a turn.
     speed: u32,
-    
+
     x: u32,
     y: u32,
-    
+
     // Can the player pass through?
     player_clip: bool,
-    
+
     //Is it good or bad?
     attacts: bool,
-    
+
     //Does it have gly-cock-mama?
     view_distance: u32,
 }
@@ -61,81 +61,104 @@ struct Room {
     level: u32,
 }
 
-
-fn printt(text_str: &str, wait: u64){
-
+fn printt(text_str: &str, wait: u64) {
     //TODO change out all callings of println!() to printt();
     thread::sleep(time::Duration::from_millis(wait));
-    println!("{}",text_str);
+    println!("{}", text_str);
 }
 
 fn do_a_wait(time: u64) {
     thread::sleep(time::Duration::from_millis(time));
 }
 
-fn menu(died: bool){
+fn menu(died: bool) {
     loop {
-    if died == true {println!("[0]Continue");};
-    printt("[1]Load\n[2]New\n[3]Options\n[4]Help\n[5]Quit", 250);
-    
-    
-    let mut input = askquest("What would you like todo?: ", 250);
-    
-    //TODO maybe figure out some way to make numbers work with ease
-    if input.trim() == "1" {input = "load".to_string();};
-    if input.trim() == "2" {input = "new".to_string();};
-    if input.trim() == "3" {input = "option".to_string();};
-    if input.trim() == "4" {input = "help".to_string();};
-    if input.trim() == "5" {input = "quit".to_string();};
-    
-    
-    // This is only enabled when the user dies
-    if died == true {
-    if input.trim() == "0" {input = "continue".to_string();};
-    };
-    
-    //TODO remove .trim()
-    if input.trim() == "load"{println!("This does not work currently"); continue;};
-    if input.trim() == "new" {newgame(); continue;};
-    if input.trim() == "option" {println!("This does not work currently"); continue;};
-    if input.trim() == "help" {println!("uh don't die");}; //TODO make help bigger
-    if input.trim() == "quit" {exit(1);};
-    if input.trim() == "continue" {printt("um hold on", 250); continue;};
-    println!("Sorry I didn't understand {}", input.to_ascii_uppercase());
-    }; //end of loop
+        if died == true {
+            println!("[0]Continue");
+        };
+        printt("[1]Load\n[2]New\n[3]Options\n[4]Help\n[5]Quit", 0);
+
+        let mut input = askquest("What would you like todo?: ", 0);
+
+        //TODO maybe figure out some way to make numbers work with ease
+        if input.trim() == "1" {
+            input = "load".to_string();
+        };
+        if input.trim() == "2" {
+            input = "new".to_string();
+        };
+        if input.trim() == "3" {
+            input = "option".to_string();
+        };
+        if input.trim() == "4" {
+            input = "help".to_string();
+        };
+        if input.trim() == "5" {
+            input = "quit".to_string();
+        };
+
+        // This is only enabled when the user dies
+        if died == true {
+            if input.trim() == "0" {
+                input = "continue".to_string();
+            };
+        };
+
+        //TODO remove .trim()
+        if input.trim() == "load" {
+            println!("This does not work currently");
+            continue;
+        };
+        if input.trim() == "new" {
+            newgame();
+            continue;
+        };
+        if input.trim() == "option" {
+            println!("This does not work currently");
+            continue;
+        };
+        if input.trim() == "help" {
+            println!("uh don't die");
+        }; //TODO make help bigger
+        if input.trim() == "quit" {
+            exit(1);
+        };
+        if input.trim() == "continue" {
+            printt("um hold on", 250);
+            continue;
+        };
+        println!("Sorry I didn't understand {}", input.to_ascii_uppercase());
+    } //end of loop
 }
 
 // This will load anymap with all the best items to test
-fn dev_map_hub(){
+fn dev_map_hub() {
     println!("Hello there where would you like to go?");
 }
 
-fn checkinv(inv: [u32; 64]){
+fn checkinv(inv: [u32; 64]) {
     println!("You have..");
     for x in 0..64 {
-    if inv[x] >= 1 {
-        println!("{0}: {1}",item_index(x),inv[x]);
+        if inv[x] >= 1 {
+            println!("{0}: {1}", item_index(x), inv[x]);
         }
     }
 }
 
-fn item_index(item_number: usize) -> String{
-
+fn item_index(item_number: usize) -> String {
     let mut index: Vec<String> = vec![String::new(); 64];
-    
+
     //TODO add a system that reads from either a text file or
     //be able to read from an external string array.
     index[0] = "Health".to_string();
     index[1] = "Mana".to_string();
     index[2] = "Armmor".to_string();
     index[3] = "Mintos".to_string();
-    
-    
+
     return index[item_number].clone();
 }
 
-
-fn give(inv: &mut [u32;64], item_number: usize, amount: u32){
+fn give(inv: &mut [u32; 64], item_number: usize, amount: u32) {
     let item_name = item_index(item_number);
     inv[item_number] = amount;
     println!("You have gained {0} {1}", amount, item_name);
@@ -153,14 +176,13 @@ fn askquest(text: &str, wait: u64) -> String {
     return input;
 }
 
-fn refresh_check(inv: &mut [u32; 64], stats: &mut Stats){
-    
+fn refresh_check(inv: &mut [u32; 64], stats: &mut Stats) {
     // To see if the player has died
     if inv[0] == 0 {
         printt("You have died", 250);
         menu(true);
     }
-    
+
     /*
     if stats[0] == 0 && stats[1] == 0 {
     printt("\nHello there, you've been sleeping some time now.\n", 500);
@@ -181,41 +203,56 @@ fn refresh_check(inv: &mut [u32; 64], stats: &mut Stats){
     */
 }
 
-fn draw_map(stats: &mut Stats, world: &mut [u8; 262144]){
-    
+fn draw_map(stats: &mut Stats, world: &mut [u8; 262144]) {
     let mut map_x: u32 = stats.player_x;
     let mut map_y: u32 = stats.player_y;
-    
-    //TODO fix this fucking mess
 
+    //TODO fix this fucking mess
+    println!("\n\n");
     for x in 0..16 {
-    print!("{}", id_index(world[cord_to_int(map_x - 8, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x - 8, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x - 7, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x - 7, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x - 6, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x - 6, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x - 5, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x - 5, map_y + 7 - x)]));
-    
-    print!("{}", id_index(world[cord_to_int(map_x - 4, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x - 4, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x - 3, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x - 3, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x - 2, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x - 2, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x - 1, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x - 1, map_y + 7 - x)]));
-    
-    print!("{}", id_index(world[cord_to_int(map_x    , map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x    , map_y + 7 - x)]));
-    
-    print!("{}", id_index(world[cord_to_int(map_x + 1, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x + 1, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x + 2, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x + 2, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x + 3, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x + 3, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x + 4, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x + 4, map_y + 7 - x)]));
-    
-    print!("{}", id_index(world[cord_to_int(map_x + 5, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x + 5, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x + 6, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x + 6, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x + 7, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x + 7, map_y + 7 - x)]));
-    print!("{}", id_index(world[cord_to_int(map_x + 8, map_y + 7 - x)]));   print!("{}", id_index(world[cord_to_int(map_x + 8, map_y + 7 - x)]));
-    
-    println!("");    
-    };
+        print!("{}", id_index(world[cord_to_int(map_x - 8, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 8, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 7, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 7, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 6, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 6, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 5, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 5, map_y + 7 - x)]));
+
+        print!("{}", id_index(world[cord_to_int(map_x - 4, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 4, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 3, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 3, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 2, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 2, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 1, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x - 1, map_y + 7 - x)]));
+
+        print!("{}", id_index(world[cord_to_int(map_x, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x, map_y + 7 - x)]));
+
+        print!("{}", id_index(world[cord_to_int(map_x + 1, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 1, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 2, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 2, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 3, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 3, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 4, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 4, map_y + 7 - x)]));
+
+        print!("{}", id_index(world[cord_to_int(map_x + 5, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 5, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 6, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 6, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 7, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 7, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 8, map_y + 7 - x)]));
+        print!("{}", id_index(world[cord_to_int(map_x + 8, map_y + 7 - x)]));
+
+        println!("");
+    }
     //io::stdout().flush().unwrap();
-    
-    
+
     /*
     for x in 0..257 {
     
@@ -236,11 +273,11 @@ fn draw_map(stats: &mut Stats, world: &mut [u8; 262144]){
 
 fn id_index(id: u8) -> String {
     let mut index: Vec<String> = vec![String::new(); 64];
-    
+
     index[0] = "█".white().to_string();
     index[1] = "█".red().to_string();
     index[2] = "█".black().to_string();
-    
+
     return index[id as usize].clone();
 }
 
@@ -249,103 +286,159 @@ fn cord_to_int(x: u32, y: u32) -> usize {
 }
 
 fn set_ent_at(world: &mut [u8; 262144], x: u32, y: u32, id: u8) {
-    let location: usize = cord_to_int(x ,y);
+    let location: usize = cord_to_int(x, y);
     world[location] = id;
 }
 
-fn render_ent(world: &mut [u8; 262144],x: u32, y: u32) {
+fn render_ent(world: &mut [u8; 262144], x: u32, y: u32) {
     println!("{}", id_index(world[cord_to_int(x, y)]));
+}
+
+fn ent_id(world: [u8; 262144], x: u32, y: u32) -> u8 {
+    return world[cord_to_int(x, y)];
 }
 
 fn update_map(mut world: &mut [u8; 262144], stats: &mut Stats) {
     // Update player
-    for x in 0..262144 {
-        if world[x] == 1 {
-            world[x] = 0;
+    for mut x in world.iter() {
+        if x == &1u8 {
+            x = &0u8;
         }
     }
+
     set_ent_at(&mut world, stats.player_x, stats.player_y, stats.player_id);
 }
 
-fn draw_line(mut world: &mut [u8; 262144], mut firstx: u32, mut firsty: u32, secondx: u32, secondy: u32, id: u8) {
-        
+fn draw_line(
+    mut world: &mut [u8; 262144],
+    mut firstx: u32,
+    mut firsty: u32,
+    secondx: u32,
+    secondy: u32,
+    id: u8,
+) {
     while firstx != secondx || firsty != secondy {
-        
         set_ent_at(&mut world, firstx, firsty, id);
-        
+
         if firstx > secondx {
             firstx -= 1;
         } else if firstx < secondx {
             firstx += 1;
         };
-        
+
         if firsty > secondy {
             firsty -= 1;
         } else if firsty < secondy {
             firsty += 1;
         };
-        
-    };
+    }
 }
 
-fn player_input(input: String, world: &mut [u8; 262144], stats: &mut Stats, inv: &mut [u32; 64]) {
-    
+fn player_input(input: &String, world: &mut [u8; 262144], stats: &mut Stats, inv: &mut [u32; 64]) {
     let words: Vec<&str> = input.trim().split(' ').collect();
 
-    // Find Verb
-    for x in words.clone() {
-        if x == "nice" {
-            print!("Cool");
-        } else if x == "kms" {
-            exit(1);
-        } else if x == "move" {
-            // Find number
-            for x in words.clone() {
-                let unit = x.parse::<u32>();
-                
-                match {
-                    Ok(var) => println!("we got a number {}", var),
-                    Err(why) => why
+    let mut amount: usize = 0;
+
+    for x in &words {
+        amount += 1;
+    }
+
+    // indexing starts at 0
+    if amount > 0 {
+        amount -= 1;
+    }
+    println!("P{}\n", amount);
+    match words[0] {
+        "nice" => print!("Cool "),
+        "move" => {
+            println!("move was called");
+
+            let mut distance: u32 = 0;
+            let direction: u8 = match words[1] {
+                "north" | "up" => 1,
+                "east" | "right" => 2,
+                "south" | "down" => 3,
+                "west" | "left" => 4,
+                _ => 0,
+            };
+            if direction == 0 {
+                println!(
+                    "You need to have move formated like:\n\n{}\n",
+                    "move DIRECTION #".red()
+                );
+                return;
+            }
+
+            for x in words {
+                if distance > 0 {
+                    break;
+                };
+                match x.parse::<u32>() {
+                    Ok(var) => distance = var,
+                    Err(ohno) => {}
+                };
+            }
+
+            if distance == 0 {
+                let mut input: String;
+                while distance == 0 {
+                    input = askquest("How far do you want to go?: ", 0);
+                    match input.trim().parse::<u32>() {
+                        Ok(var) => {
+                            distance = var;
+                            continue;
+                        }
+                        Err(ohno) => {
+                            println!("That isn't a number \n{0} {1}", "ERROR:".red(), ohno)
+                        }
+                    };
                 }
             };
-        };
+        }
+        "kms" => exit(1),
+        "help" => println!(
+            "FQA:\n1. Why did my command not work?: You maybe entered it wrong or too many"
+        ),
+        _ => println!("sorry I didn't understand"),
     };
-    
-    
 }
 
-fn newgame(){
-
+fn newgame() {
     // Inv that will be passed around the game
     let mut inv: [u32; 64] = [0; 64];
-    
-    let mut stats = Stats { player_x: 37, player_y: 56, player_name: String::new(), player_level: 0, player_id: 1};
-    
+
+    let mut stats = Stats {
+        player_x: 37,
+        player_y: 56,
+        player_name: String::new(),
+        player_level: 0,
+        player_id: 1,
+    };
+
     let mut world: [u8; 262144] = [0; 262144];
     // Health isn't going to be in the stats because it will always be called
     // When you look at your invitory
     // give(&mut inv, 0, 100);
     inv[0] = 100;
-    
+
     let mut input: String = String::new();
-    
+
     // Borders of the world
     //TODO move these to the world gen
     draw_line(&mut world, 1, 1, 255, 1, 2);
-    draw_line(&mut world, 1,   1, 1, 255,   2);
+    draw_line(&mut world, 1, 1, 1, 255, 2);
     draw_line(&mut world, 1, 255, 255, 255, 2);
     draw_line(&mut world, 255, 1, 255, 255, 2);
-    
-    
+
+    set_ent_at(&mut world, 37, 60, 2);
+
     loop {
         input = String::new();
         update_map(&mut world, &mut stats);
         draw_map(&mut stats, &mut world);
-        input = askquest("-What would you like to do?: ", 250);
-        
-        player_input(input, &mut world, &mut stats, inv);
-    };
+        input = askquest("\n-What would you like to do?: ", 0);
+        player_input(&input, &mut world, &mut stats, &mut inv);
+    }
     //TODO needs updating --> refresh_check(&mut inv, &mut stats);
     exit(1);
 }
-
